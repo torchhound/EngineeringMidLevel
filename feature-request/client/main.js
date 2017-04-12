8367a6e279ee5c7807026153bf51a111786bb0e0
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2017-04-12 18:35:23 -0400

    Client web page displays db entries, formdoes not currently submit to db, form is too wide, radio checkboxes only allow a choice between last 3, first is permanently selected

--- feature-request/client/main.js
+++ feature-request/client/main.js
@@ -1,22 +1,47 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+import { Features } from '../imports/api/features.js';
+
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+Template.requestForm.rendered = function() {
+	this.$('.ui.radio.checkbox').checkbox();
+};
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.body.helpers({
+  features() {
+    return Features.find({});
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.body.events({
+	'submit .requestForm'(event) {
+    	event.preventDefault();
+    	const target = event.target;
+    	const title = target.title.value;
+    	const description = target.description.value;
+    	const client = target.client.value;
+    	const clientPriority = target.clientPriority.value;
+    	const targetDate = target.targetDate.value;
+    	const ticketUrl = target.ticketUrl.value;
+    	const productArea = target.productArea.value;
+    	Features.insert({
+      		title,
+      		description,
+      		client,
+      		clientPriority,
+      		targetDate,
+      		ticketUrl,
+      		productArea,
+      		createdAt: new Date(),
+    	});
+		target.title.value = '';
+		target.description.value = '';
+		target.client.value = '';
+		target.clientPriority.value = '';
+		target.targetDate.value = '';
+		target.ticketUrl.value = '';
+		//clear productArea?
+  	},
 });
